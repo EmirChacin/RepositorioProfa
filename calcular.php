5f277a406640018460655e84114651fc1236a825
Emir <emiralexnader@gmail.com>
2020-07-13 17:41:02 -0500

    se actualiza calcular.php

--- calcular.php
+++ calcular.php
@@ -6,12 +6,12 @@ $flete = $numero_cajas*50;
 if ($cantidad>1000)
     $descuento=$subtotal*0.15;
 else
-{
+
     if($cantidad>100)
         $descuento=$subtotal*0.05;
     else
         $descuento=0;
-}
+
 $total=$subtotal+$flete-$descuento;
 echo "El precio total a pagar es: ".$total."<br>";
 echo "El monto por flete es: ".$flete."<br>";
